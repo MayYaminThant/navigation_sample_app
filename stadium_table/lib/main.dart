8c109a468308d2062bb5e54fce483f7b2892fa4e
MayYaminThant <mayyamin17420@gmail.com>
2023-12-14 23:01:21 +0630

    scroll table with border radius

--- stadium_table/lib/main.dart
+++ stadium_table/lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
@@ -31,64 +32,112 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey,
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: DataTable2(
-        horizontalScrollController: ScrollController(),
-          columnSpacing: 12,
-          horizontalMargin: 12,
-          columns: const [
-            DataColumn2(
-              label: Text('Column A'),
-              size: ColumnSize.L,
+      body: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(22),
+        ),
+        margin: const EdgeInsets.all(20),
+        padding: const EdgeInsets.all(20),
+        child: Theme(
+          data: Theme.of(context).copyWith(
+            cardColor: Colors.transparent,
+            dividerColor: Colors.transparent,
+            cardTheme: CardTheme(
+              color: Colors.transparent,
+              shadowColor: Colors.transparent,
+              elevation: 0, // remove shadow
+              margin: const EdgeInsets.all(0),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(22),
+              ),
             ),
-            DataColumn(
-              label: Text('Column B'),
+          ),
+          child: SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            physics: const AlwaysScrollableScrollPhysics(),
+            child: Container(
+              constraints: BoxConstraints(
+                maxWidth: MediaQuery.of(context).size.width,
+                maxHeight: MediaQuery.of(context).size.height,
+              ),
+              decoration: BoxDecoration(
+                color: Colors.transparent,
+                borderRadius: BorderRadius.circular(22),
+              ),
+              child: DataTable2(
+                  horizontalScrollController: ScrollController(),
+                  headingRowColor:
+                      MaterialStateColor.resolveWith((states) => Colors.blue),
+                  columnSpacing: 12,
+                  horizontalMargin: 12,
+                  headingRowDecoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(22),
+                  ),
+                  // border: TableBorder.all(
+                  //   borderRadius: BorderRadius.circular(22),
+                  //   color: Colors.transparent,
+                  // ),
+                  columns: const [
+                    DataColumn2(
+                      label: Text('Column A'),
+                      size: ColumnSize.L,
+                    ),
+                    DataColumn(
+                      label: Text('Column B'),
+                    ),
+                    DataColumn(
+                      label: Text('Column C'),
+                    ),
+                    DataColumn(
+                      label: Text('Column D'),
+                    ),
+                    DataColumn(
+                      label: Text('Column E'),
+                      numeric: true,
+                    ),
+                    DataColumn(
+                      label: Text('Column F'),
+                      numeric: true,
+                    ),
+                    DataColumn(
+                      label: Text('Column G'),
+                      numeric: true,
+                    ),
+                    DataColumn(
+                      label: Text('Column H'),
+                      numeric: true,
+                    ),
+                    DataColumn(
+                      label: Text('Column I'),
+                      numeric: true,
+                    ),
+                  ],
+                  rows: List<DataRow>.generate(
+                      100,
+                      (index) => DataRow(cells: [
+                            DataCell(Text('A' * (10 - index % 10))),
+                            DataCell(Text('B' * (10 - (index + 5) % 10))),
+                            DataCell(Text('C' * (15 - (index + 5) % 10))),
+                            DataCell(Text('D' * (15 - (index + 10) % 10))),
+                            DataCell(Text('E' * (10 - (index + 5) % 10))),
+                            DataCell(Text('F' * (15 - (index + 5) % 10))),
+                            DataCell(Text('G' * (15 - (index + 10) % 10))),
+                            DataCell(Text('H' * (15 - (index + 10) % 10))),
+                            DataCell(Text(((index + 0.1) * 25.4).toString()))
+                          ]))),
             ),
-            DataColumn(
-              label: Text('Column C'),
-            ),
-            DataColumn(
-              label: Text('Column D'),
-            ),
-            DataColumn(
-              label: Text('Column E'),
-              numeric: true,
-            ),
-            DataColumn(
-              label: Text('Column F'),
-              numeric: true,
-            ),
-            DataColumn(
-              label: Text('Column G'),
-              numeric: true,
-            ),
-            DataColumn(
-              label: Text('Column H'),
-              numeric: true,
-            ),
-            DataColumn(
-              label: Text('Column I'),
-              numeric: true,
-            ),
-          ],
-          rows: List<DataRow>.generate(100, (index) => DataRow(cells: [
-                DataCell(Text('A' * (10 - index % 10))),
-                DataCell(Text('B' * (10 - (index + 5) % 10))),
-                DataCell(Text('C' * (15 - (index + 5) % 10))),
-                DataCell(Text('D' * (15 - (index + 10) % 10))),
-                DataCell(Text('E' * (10 - (index + 5) % 10))),
-                DataCell(Text('F' * (15 - (index + 5) % 10))),
-                DataCell(Text('G' * (15 - (index + 10) % 10))),
-                DataCell(Text('H' * (15 - (index + 10) % 10))),
-                DataCell(Text(((index + 0.1) * 25.4).toString()))
-              ]))),// This trailing comma makes auto-formatting nicer for build methods.
+          ),
+        ),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
