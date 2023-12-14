5dc18b5a34b86b13f26650974080adb8e26a3289
kohtunaunglinn <kohtunaunglinn@gmail.com>
2023-12-14 22:27:21 +0630

    stadium table sample data

--- stadium_table/lib/main.dart
+++ stadium_table/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:data_table_2/data_table_2.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Stadium Table Demo Page'),
     );
   }
 }
@@ -38,9 +39,56 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Text(''),
-      ),// This trailing comma makes auto-formatting nicer for build methods.
+      body: DataTable2(
+        horizontalScrollController: ScrollController(),
+          columnSpacing: 12,
+          horizontalMargin: 12,
+          columns: const [
+            DataColumn2(
+              label: Text('Column A'),
+              size: ColumnSize.L,
+            ),
+            DataColumn(
+              label: Text('Column B'),
+            ),
+            DataColumn(
+              label: Text('Column C'),
+            ),
+            DataColumn(
+              label: Text('Column D'),
+            ),
+            DataColumn(
+              label: Text('Column E'),
+              numeric: true,
+            ),
+            DataColumn(
+              label: Text('Column F'),
+              numeric: true,
+            ),
+            DataColumn(
+              label: Text('Column G'),
+              numeric: true,
+            ),
+            DataColumn(
+              label: Text('Column H'),
+              numeric: true,
+            ),
+            DataColumn(
+              label: Text('Column I'),
+              numeric: true,
+            ),
+          ],
+          rows: List<DataRow>.generate(100, (index) => DataRow(cells: [
+                DataCell(Text('A' * (10 - index % 10))),
+                DataCell(Text('B' * (10 - (index + 5) % 10))),
+                DataCell(Text('C' * (15 - (index + 5) % 10))),
+                DataCell(Text('D' * (15 - (index + 10) % 10))),
+                DataCell(Text('E' * (10 - (index + 5) % 10))),
+                DataCell(Text('F' * (15 - (index + 5) % 10))),
+                DataCell(Text('G' * (15 - (index + 10) % 10))),
+                DataCell(Text('H' * (15 - (index + 10) % 10))),
+                DataCell(Text(((index + 0.1) * 25.4).toString()))
+              ]))),// This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
